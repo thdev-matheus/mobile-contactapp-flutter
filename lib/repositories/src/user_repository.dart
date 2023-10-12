d055d8f8600b74a39f968521990ab61849078abb
Theus <matheusth.dev@gmail.com>
2023-10-12 16:35:33 -0300

    feat: user repository and register function

---
+++ lib/repositories/src/user_repository.dart
@@ -0,0 +1,32 @@
+import 'package:contact_app/api/contact_app_api.dart';
+import 'package:dio/dio.dart';
+
+class UserRepository {
+  final _api = ContactAppAPI().dio;
+
+  Future<void> register(
+      {required String username,
+      required String password,
+      required String imagePath}) async {
+    try {
+      _api.options.headers['X-Parse-Revocable-Session'] = '1';
+
+      await _api.post('/users', data: {
+        'username': username,
+        'password': password,
+        'imagePath': imagePath,
+      });
+    } on DioException catch (e) {
+      throw DioException(
+        requestOptions: e.requestOptions,
+        response: e.response,
+        stackTrace: e.stackTrace,
+        error: e.error,
+        message: e.message,
+        type: e.type,
+      );
+    } catch (_) {
+      throw Exception();
+    }
+  }
+}
