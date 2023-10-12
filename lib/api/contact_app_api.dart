0a3c13de9b69bcc3dd3fa82a6b21f8a185636646
Theus <matheusth.dev@gmail.com>
2023-10-12 16:30:52 -0300

    feat: api dio config

---
+++ lib/api/contact_app_api.dart
@@ -0,0 +1,16 @@
+import 'package:contact_app/api/contact_app_interceptor.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ContactAppAPI {
+  final _dio = Dio();
+
+  Dio get dio => _dio;
+
+  ContactAppAPI() {
+    _dio.options.baseUrl = dotenv.get('CONTACT_APP_BASE_URL');
+    _dio.options.headers['Content-Type'] = 'applicatioon/json';
+
+    _dio.interceptors.add(ContactAppAPIInterceptor());
+  }
+}
